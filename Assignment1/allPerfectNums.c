cd19b5f32208ffb337de330100d965b9cb1df953
Daniel Goel <95840481+DanielGoel@users.noreply.github.com>
2023-01-11 16:31:06 -0500

    2

--- Assignment1/allPerfectNums.c
+++ Assignment1/allPerfectNums.c
@@ -2,15 +2,21 @@
 #include <string.h>
 #include <math.h>
 int main(){
+    printf("the perfect numbers are: ");
     int n, sum, remainder;
+
+
     for (n=0; n<10000; n++){
+
+
         for (int i=1; i<n; i++){
             remainder= n%i;
             if (remainder==0)
                 sum += i;   
         }
+
         if (sum==n)
-            printf("%d  ",sum);
+            printf(" %d",sum);
     }
     return 0;
 }
